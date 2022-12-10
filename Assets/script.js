329597502775c7176a58028f3000295e3a4468e6
ColtonTO <colton1105@gmail.com>
2022-12-10 12:51:30 +0400

    added comments and alert window when saving appointment

--- Assets/script.js
+++ Assets/script.js
@@ -1,19 +1,19 @@
 $( document ).ready(function() {
-  //
-  $(function () {
-    $("#currentDay").text(dayjs().format('dddd, MMMM YYYY'))
+  // Below is the function used to determine current time and day
+    $("#currentDay").text(dayjs().format('dddd MMMM d, YYYY'))
 
     const rows = document.getElementsByClassName("row");
     let currentTime = parseInt(dayjs().format('H'));
-  //
+  // Below pushes saved content into localStorage and pulls up an alert to let user know their appointment was saved into localstorage. 
    $(".saveBtn").on("click", function () {
       
       console.log(this);
       var text = $(this).siblings(".description").val();
       var time = $(this).parent().attr("id");
       localStorage.setItem(time, text);
+      return alert("Appointment Added to localstorage✅")
   })
-  //
+  // This pulls said hours and their description from localStorage
   $("#9 .description").val(localStorage.getItem("9"));
   $("#10 .description").val(localStorage.getItem("10"));
   $("#11 .description").val(localStorage.getItem("11"));
@@ -24,7 +24,7 @@ $( document ).ready(function() {
   $("#16 .description").val(localStorage.getItem("16"));
   $("#17 .description").val(localStorage.getItem("17"));
 
-//
+// Function for setting the background colors based on current time and specified row time
   function timer() {
      
       Array.from(rows).forEach(row => {
@@ -46,11 +46,10 @@ $( document ).ready(function() {
           }
         }
       });
-      //
+      // Function that declares background color changes
       function setColor(element, color) {
         element.style.backgroundColor = color;
       }
     }
   timer();
 });
-})
