783a34f4115c24d5ed6e2c85b766c36b1b441c84
ColtonTO <colton1105@gmail.com>
2022-12-10 00:25:41 -0800

    working local storage, optomized JS

--- Assets/script.js
+++ Assets/script.js
@@ -1,55 +1,56 @@
+$( document ).ready(function() {
+  //
+  $(function () {
+    $("#currentDay").text(dayjs().format('dddd, MMMM YYYY'))
 
+    const rows = document.getElementsByClassName("row");
+    let currentTime = parseInt(dayjs().format('H'));
+  //
+   $(".saveBtn").on("click", function () {
+      
+      console.log(this);
+      var text = $(this).siblings(".description").val();
+      var time = $(this).parent().attr("id");
+      localStorage.setItem(time, text);
+  })
+  //
+  $("#9 .description").val(localStorage.getItem("9"));
+  $("#10 .description").val(localStorage.getItem("10"));
+  $("#11 .description").val(localStorage.getItem("11"));
+  $("#12 .description").val(localStorage.getItem("12"));
+  $("#13 .description").val(localStorage.getItem("13"));
+  $("#14 .description").val(localStorage.getItem("14"));
+  $("#15 .description").val(localStorage.getItem("15"));
+  $("#16 .description").val(localStorage.getItem("16"));
+  $("#17 .description").val(localStorage.getItem("17"));
 
-
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-$(function () {
-  $("#currentDay").text(dayjs().format('dddd, MMMM YYYY'))
-
-const rows = document.getElementsByClassName("row");
-let currentTime = parseInt(dayjs().format('H'));
-
-Array.from(rows).forEach(row => {
-  let
-    rowIdString = row.id,
-    rowHour;
-  if (rowIdString) {
-    rowHour = parseInt(rowIdString);
-  }
-  if (rowHour) {
-    if (currentTime === rowHour) {
-      setColor(row, "red");
-    } else if ((currentTime < rowHour) && (currentTime > rowHour - 6)) {
-      setColor(row, "green");
-    } else if ((currentTime > rowHour) && (currentTime < rowHour + 6)) {
-      setColor(row, "lightgrey");
-    } else {
-      setColor(row, "white");
+//
+  function timer() {
+     
+      Array.from(rows).forEach(row => {
+        let
+          rowIdString = row.id,
+          rowHour;
+        if (rowIdString) {
+          rowHour = parseInt(rowIdString);
+        }
+        if (rowHour) {
+          if (currentTime === rowHour) {
+            setColor(row, "tomato");
+          } else if ((currentTime < rowHour) && (currentTime > rowHour - 6)) {
+            setColor(row, "lightgreen");
+          } else if ((currentTime > rowHour) && (currentTime < rowHour + 6)) {
+            setColor(row, "lightgrey");
+          } else {
+            setColor(row, "white");
+          }
+        }
+      });
+      //
+      function setColor(element, color) {
+        element.style.backgroundColor = color;
+      }
     }
-  }
+  timer();
 });
-
-function setColor(element, color) {
-  element.style.backgroundColor = color;
-}
-
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
-});
+})
